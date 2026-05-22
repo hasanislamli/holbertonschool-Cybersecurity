bc73c67f5da3a6eb468d7237d76547071755c253
hasanislamli <13733@holbertonstudents.com>
2026-05-22 07:22:20 +0000

    adding cidr

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-c=$1; for i in 1 2 3 4; do n=$(( c>8 ? 8 : c )); printf "%d" $((256 - 2**(8-n))); [ $i -lt 4 ] && printf "."; (( c-=n )); done; echo
+c=$1; for i in 1 2 3 4; do n=$((c>8?8:c)); m=$((n==8?255:256-2**(8-n))); printf "%d" $m; [ $i -lt 4 ] && printf "."; ((c-=n)); done; echo
